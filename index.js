dcd9124d514318ed853040948ce8988daf460e97
Richard Carls <richard.j.carls@gmail.com>
2016-07-22 18:14:00 -0400

    Added entryToCite
    - Takes an MF2 h-entry item and converts it to h-cite

--- index.js
+++ index.js
@@ -108,3 +108,21 @@ module.exports.toJf2 = function(mf2) {
   }
 
 };
+
+
+/**
+ * Formats an h-entry to an h-cite
+ * 
+ * Useful for saving responses from a webmention.
+ * 
+ * @param {Object} entry - The Microformats2 parsed entry
+ * @returns {Object} - The entry formatted as an h-cite
+ */
+module.exports.entryToCite = function(entry) {
+  return {
+    type: ['h-cite'],
+    properties: _.pick(entry.properties, [
+      'name', 'url', 'uid', 'author', 'content', 'published', 'accessed', 'publication',
+    ]),
+  };
+};
