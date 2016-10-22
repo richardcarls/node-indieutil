b94448e669c5e6a77ddc7d7195bfefc91b755cf0
Richard Carls <richard.j.carls@gmail.com>
2016-10-22 12:58:46 -0400

    jf2FetchRefs: Include references from fetched item

--- test/jf2-fetch-refs.test.js
+++ test/jf2-fetch-refs.test.js
@@ -7,7 +7,7 @@ var nock = require('nock');
 
 var jf2FetchRefs = require('../lib/jf2-fetch-refs');
 
-var mockTargetNoteUrl = 'https://example.com/notes';
+var mockTargetNoteUrl = 'https://example.com';
 
 describe('@rcarls/indieutil.jf2FetchRefs', function() {
 
@@ -15,9 +15,9 @@ describe('@rcarls/indieutil.jf2FetchRefs', function() {
     
     var reply = {
       type: 'entry',
-      'in-reply-to': mockTargetNoteUrl + '/1',
+      'in-reply-to': mockTargetNoteUrl + '/notes/test',
       name: 'Test Reply',
-      url: mockTargetNoteUrl + '/2',
+      url: mockTargetNoteUrl + '/notes/reply',
       content: 'lorem ipsum',
       published: '2015-10-21T12:00:00-0700',
     };
@@ -28,8 +28,8 @@ describe('@rcarls/indieutil.jf2FetchRefs', function() {
 
       before('mock the target note response', function() {
         nock(mockTargetNoteUrl)
-          .get('/1')
-          .replyWithFile(200, __dirname + '/mocks/test-note.html');
+          .get('/notes/test')
+          .replyWithFile(200, __dirname + '/mocks/test-note-2.html');
       });
       
       before('fetch references', function(done) {
@@ -54,73 +54,53 @@ describe('@rcarls/indieutil.jf2FetchRefs', function() {
         expect(nock.isDone()).to.be.true;
       });
 
+      it('should return the original object', function() {
+        expect(result).to.have.property('type', 'entry');
+        expect(result).to.have
+          .property('in-reply-to', mockTargetNoteUrl + '/notes/test');
+        expect(result).to.have.property('name', 'Test Reply');
+        expect(result).to.have
+          .property('url', mockTargetNoteUrl + '/notes/reply');
+        expect(result).to.have.property('content', 'lorem ipsum');
+        expect(result).to.have
+          .property('published', '2015-10-21T12:00:00-0700');
+      });
+
       it('should contain a references object', function() {
         expect(result.references).to.be.an('object');
       });
 
       it('should have the reply URL as reference key', function() {
-        expect(result.references).to.have.property(mockTargetNoteUrl + '/1');
+        expect(result.references).to.have
+          .property(mockTargetNoteUrl + '/notes/test');
       });
 
       it('should contain the complete reference', function() {
-        var ref = result.references[mockTargetNoteUrl + '/1'];
+        var ref = result.references[mockTargetNoteUrl + '/notes/test'];
         
         expect(ref.type).to.equal('cite');
-        expect(ref.name).to.equal('Test Note');
         expect(ref.content).to.deep.equal({
           'content-type': 'text/html',
-          value: 'lorem ipsum',
+          value: '\n        <p><strong>Lorem ipsum</strong> dolar sit amet.</p>\n\n        <p>End.</p>\n      ',
         });
-        expect(ref.url).to.equal(mockTargetNoteUrl + '/1');
+        expect(ref.url).to.equal(mockTargetNoteUrl + '/notes/test');
       });
 
-      it('should contain a properly formatted accessed date', function() {
-        var ref = result.references[mockTargetNoteUrl + '/1'];
-        
-        expect(ref.accessed).to.match(
-            /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/
-        );
+      it('should have the reply author URL as reference key', function() {
+        expect(result.references).to.have
+          .property('https://example.com/');
+      });
+
+      it('should contain the complete eauthor reference', function() {
+        var ref = result.references['https://example.com/'];
+
+        expect(ref.type).to.equal('card');
+        expect(ref.photo).to.equal('https://example.com/images/author.jpg');
+        expect(ref.name).to.equal('Test Author');
+        expect(ref.url).to.equal('https://example.com/');
       });
 
     }); // fetching with default options
-
-    describe('fetching and embedding references', function() {
-
-      var result;
-
-      before('mock the target note response', function() {
-        nock(mockTargetNoteUrl)
-          .get('/1')
-          .replyWithFile(200, __dirname + '/mocks/test-note.html');
-      });
-      
-      before('fetch references', function(done) {
-        jf2FetchRefs(reply, {
-          embedReferences: true,
-        })
-          .then(function(jf2) {
-            result = jf2;
-            
-            done();
-          });
-      });
-      
-      it('should not contain a references property', function() {
-        expect(result.references).to.not.be.ok;
-      });
-
-      it('should replace the URL with an embedded item', function() {
-        expect(result['in-reply-to'].type).to.equal('cite');
-        expect(result['in-reply-to'].name).to.equal('Test Note');
-        expect(result['in-reply-to'].content).to.deep.equal({
-          'content-type': 'text/html',
-          value: 'lorem ipsum',
-        });
-        expect(result['in-reply-to'].url)
-          .to.equal(mockTargetNoteUrl + '/1');
-      });
-
-    }); // fetching and embedding references
     
   }); // a simply reply note
 
